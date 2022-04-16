2017ed7e7962cdb6892bc34e7ed8d163bb8542c6
namnguyen16 <103294654+namnguyen16@users.noreply.github.com>
2022-04-16 11:12:27 +0700

    Delete FirstDialog.java

--- FirstDialog.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.JOptionPane;
-public class FirstDialog{
-   public static void main(String[] arg){
-       JOptionPane.showMessageDialog(null,"Hello world! How are you?");
-   }
-}
